020b1a948d5bd059c746562ccc08c82c768c8715
Adriano Moreira <adri3d@gmail.com>
2020-01-04 15:57:48 -0300

    add problem 1 in java

---
+++ project-euler-100-java/src/main/java/com/github/adrianomoreira/projecteuler100/Problem001.java
@@ -0,0 +1,32 @@
+package com.github.adrianomoreira.projecteuler100;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
+
+public class Problem001 {
+
+    public static int multiplesOf3And5Java7(int number) {
+        List<Integer> multiples = new ArrayList<Integer>();
+        for (int i = 1; i < number; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                multiples.add(i);
+            }
+        }
+        Integer sum = 0;
+        for (Integer multiple : multiples) {
+            sum += multiple;
+        }
+        return sum;
+    }
+
+    public static int multiplesOf3And5(int number) {
+        IntPredicate isMultiplesOf3Or5 = (value) -> value % 3 == 0 || value % 5 == 0;
+        return IntStream
+                .range(1, number)
+                .filter(isMultiplesOf3Or5)
+                .reduce(Integer::sum).getAsInt();
+    }
+
+}
