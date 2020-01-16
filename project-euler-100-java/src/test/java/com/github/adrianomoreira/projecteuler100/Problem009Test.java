c8632cefba803f94331f44e6fffda1a536b45927
Adriano Moreira <adri3d@gmail.com>
2020-01-15 22:18:00 -0300

    add problem 9 java

--- project-euler-100-java/src/test/java/com/github/adrianomoreira/projecteuler100/Problem009Test.java
+++ project-euler-100-java/src/test/java/com/github/adrianomoreira/projecteuler100/Problem009Test.java
@@ -4,15 +4,27 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class Problem009Test {
+
+    @DisplayName("specialPythagoreanTriplet(24) should return 31875000")
+    @Test
+    public void test100() {
+        Assertions.assertEquals(31875000, Problem009.specialPythagoreanTriplet(100));
+    }
+
     @DisplayName("specialPythagoreanTriplet(24) should return 480")
     @Test
-    public void test24(){
+    public void test24() {
         Assertions.assertEquals(480, Problem009.specialPythagoreanTriplet(24));
     }
 
-//    specialPythagoreanTriplet(1000) should return 31875000.
-//    specialPythagoreanTriplet(24) should return 480.
-//    specialPythagoreanTriplet(120) should return 49920, 55080 or 60000
-
+    @DisplayName("specialPythagoreanTriplet(120) should return 49920, 55080 or 60000")
+    @Test
+    public void test120() {
+        var response = Problem009.specialPythagoreanTriplet(120);
+        List<Integer> valid = List.of(49920, 55080, 60000);
+        Assertions.assertTrue(valid.contains(response));
+    }
 }
